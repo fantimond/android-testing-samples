b92d4549af0699db7ec038a7aeaccb029e9c6f41
liujianhui <liujianhui@kuaishou.com>
2020-01-13 15:32:35 +0800

    minor code tweak to BasicSample(Espresso)

--- ui/espresso/BasicSample/app/src/main/java/com/example/android/testing/espresso/BasicSample/MainActivity.java
+++ ui/espresso/BasicSample/app/src/main/java/com/example/android/testing/espresso/BasicSample/MainActivity.java
@@ -62,8 +62,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
           mTextView.setText(text);
         } else if (view.getId() == activityChangeTextBtnId) {
           // Second button's interaction: start an activity and send a message to it.
-          Intent intent = com.example.android.testing.espresso.BasicSample.ShowTextActivity
-              .newStartIntent(this, text);
+          Intent intent = ShowTextActivity.newStartIntent(this, text);
           startActivity(intent);
         }
     }
